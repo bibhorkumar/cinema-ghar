a7169f3a144c3947da35cadf89bab6d623788aba
Bibhor Kumar <80465500+bibhorkumar@users.noreply.github.com>
2023-01-23 22:58:01 +0530

    minor UI change for mobiles

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Headers({inputRef,searchQuery, handleKeyDown}){
     return(
         <header className="header">
-            <h2>Cinema Ghar</h2>
+            <h1>Cinema Ghar</h1>
             <input type="text" placeholder="🔍search" className="search-bar" ref={inputRef}  onKeyDown={handleKeyDown}/>
         </header>
     )
