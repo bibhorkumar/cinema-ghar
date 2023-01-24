4a7f3d51b8b9f74ab36266cde752424c448f5e1f
Bibhor Kumar <80465500+bibhorkumar@users.noreply.github.com>
2023-01-24 23:33:33 +0530

    dark mode in search added

--- src/components/ResultsModal.js
+++ src/components/ResultsModal.js
@@ -1,11 +1,15 @@
 import React from "react";
 import PopularList from "./PopularList";
+import ThemeContext from "../ThemeContext";
 
 
 function ResultsModal({query,searchResults,closeModal}) {
+  const darkMode= React.useContext(ThemeContext)
+  const modalBackgroundClassName = darkMode ? "modal-background body-dark" : "modal-background";
+  const modalContainerClassName = darkMode ? "modal-container sec-hed-dark" : "modal-container";
   return (
-    <div className="modal-background">
-      <div className="modal-container">
+    <div className={modalBackgroundClassName}>
+      <div className={modalContainerClassName}>
         <div className="modal-title">
           <h2>{`Results for ${query}....`}</h2>
           <button className="modal-close-btn" onClick={closeModal}>X</button>
